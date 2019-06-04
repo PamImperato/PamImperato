5c309fe3913f238fe91108657a2fdc0d4b7946a8
PamImperato <51248410+PamImperato@users.noreply.github.com>
2019-06-04 16:03:38 -0400

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("STU00030191 Unit1CurrencyConverteCR")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Toshiba")]
+[assembly: AssemblyProduct("STU00030191 Unit1CurrencyConverteCR")]
+[assembly: AssemblyCopyright("Copyright © Toshiba 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91c26039-fbf5-4848-a18e-43f97f5d8317")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Program Sun Ev Currency convert.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STU00030191_Unit1CurrencyConverteCR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {;
+            Console.Write("Please Enter a number");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please Enter a second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter yet a third and final number, please");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            double sum = num + Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Total=" + sum);
+            Console.ReadKey();
+
+            //Currency from Sum
+            Console.WriteLine("Amount in US dollars = {0,C}", sum);
+            Console.WriteLine("Amount in Yen = {0:JPY}", sum);
+            Console.WriteLine("Amount in Krona = {0:SEK}", sum);
+            Console.WriteLine("Amount in Baht = {0:THB}", sum);
+
+            //min max average
+            double[] myarray = { num + num2 + num3 };
+
+           
+
+
+            Console.WriteLine ("The average value is");
+            Console.WriteLine(myarray.Average());
+
+            //min
+            Console.WriteLine("The minimum number is");
+            Console.WriteLine(myarray.Min());
+
+            //Max
+            Console.WriteLine("The maximum number is");
+            Console.WriteLine(myarray.Max());
+
+            
+
+
+
+
+
+
+
+
+            
+
+            
+
+
+
+
+        }
+    }
+}
